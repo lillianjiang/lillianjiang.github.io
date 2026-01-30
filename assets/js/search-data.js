aab41289322cdba477dc12c6d0b54c3f0fb92a4b
lillianjiang <43865031+lillianjiang@users.noreply.github.com>
2026-01-30 08:45:55 +0000

    Deploying to gh-pages from @ lillianjiang/lillianjiang.github.io@f8630f88a0cc0462c8a9c7c1c0a1cd1bf3d008b0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -483,7 +483,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/cv_20251030.pdf", "_blank");
         },
       },{
         id: 'social-email',
